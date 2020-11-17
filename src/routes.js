8f3b9259d41479e8b4e50c80d3714f08c2438c78
ricardoimagure <ricardo.imagure@gmail.com>
2020-11-17 17:48:51 -0300

    Removes Home Builder

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
-import HomeBuilder from './containers/HomeBuilder/HomeBuilder';
+import Home from './components/Home/Home';
 import ArenaRankingBuilder from './containers/ArenaRankingBuilder/ArenaRankingBuilder';
 import BossRankingBuilder from './containers/BossRankingBuilder/BossRankingBuilder';
 import HallOfFameBuilder from './containers/HallOfFameBuilder/HallOfFameBuilder';
@@ -14,7 +14,7 @@ class Routes extends Component {
       <BrowserRouter>
         <Switch>
           <Layout>
-            <Route exact path="/" component={HomeBuilder} />
+            <Route exact path="/" component={Home} />
             <Route exact path="/arena_ranking" component={ArenaRankingBuilder} />
             <Route exact path="/boss_ranking" component={BossRankingBuilder} />
             <Route exact path="/hall_of_fame" component={HallOfFameBuilder} />
