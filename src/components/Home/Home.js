fe5dbc47f87780990d1019192a3d5fe9179dca94
ricardoimagure <ricardo.imagure@gmail.com>
2020-11-17 18:10:57 -0300

    Adds message at Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,16 @@ const Home = props => {
     <div>
 
       <div className={classes.Home}>
-        Global Home Page
+      Hello everyone, and welcome to the unofficial Knighthood Leaderboard website.
+      <br/>
+      <br/>
+      With this website, we intend to have a place where we can compete in all aspects of this loved game of ours, and have fun as a community.
+      <br/>
+      <br/>
+      We are adding new leaderboards as we find time and ideas. So if there is something you think it would be nice to be seen in here, please let us know.
+      <br/>
+      <br/>
+      When submiting a guild boss picture, please be sure to get a image from you in game guild leaderboar. In this way, we can garantee that we are registering the score in the right Boss, and to the right player.
       </div>
 
     </div>
