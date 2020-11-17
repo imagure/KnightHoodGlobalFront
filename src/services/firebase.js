c3d1123477f4a9a2478bf8cc0f07d380d10eeeac
ricardoimagure <ricardo.imagure@gmail.com>
2020-11-17 14:22:50 -0300

    Makes a temporary correction over sheet ID. Refactor it later!

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/storage'
 
-console.log("Firebase config: ", process.env.REACT_APP_FIREBASE_CONFIG);
-
 firebase.initializeApp(JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG));
 
 const storage = firebase.storage()
